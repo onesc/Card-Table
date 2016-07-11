11f855d7005911c2e0899956b70b4ed68e76798f
onesc <onescyt@gmail.com>
2016-07-11 20:57:52 +1000

    draw cards to table

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -3,10 +3,19 @@ var cards = [];
 
 var card = new DragImage('/images/ace.png', 300, 50);
 cards.push(card);
-var card = new DragImage('https://assets.servedby-buysellads.com/p/manage/asset/id/24209', 400, 200);
-cards.push(card);
 var card = new DragImage('https://i.ytimg.com/vi/5UTWBLOuW8s/hqdefault.jpg?custom=true&w=320&h=180&stc=true&jpg444=true&jpgq=90&sp=68&sigh=kZ_jfTgIkbVzZDOBFI8gXRlsTQE', 400, 200);
 cards.push(card);
+
+function drawCard(deck) {
+  card = deck.pop();
+  return card;
+}
+
+function cardToHand() {
+  var newCard = drawCard(deck);
+  var card = new DragImage(newCard.src, 300, 300);
+  cards.push(card);
+}
 
 var handLine = new Image();
 handLine.src = "/images/line.png";
--- public/js/card.js
+++ public/js/card.js
@@ -7,22 +7,22 @@ function initDeck () {
       var card = {};
       if (i <= 10) {
       card.name = i + ' of Hearts';
-      card.src = imageDir + i + '_of_hearts';
+      card.src = imageDir + i + '_of_hearts.png';
       deck.push(card);
     } else {
       var card = {};
       if (i == 11) {
         card.name = "Jack of Hearts";
-        card.src = imageDir + 'jack_of_hearts';
+        card.src = imageDir + 'jack_of_hearts.png';
       } else if (i == 12) {
         card.name = "Queen of Hearts";
-          card.src = imageDir + 'queen_of_hearts';
+          card.src = imageDir + 'queen_of_hearts.png';
       } else if (i == 13) {
         card.name = "King of Hearts";
-          card.src = imageDir + 'king_of_hearts';
+          card.src = imageDir + 'king_of_hearts.png';
       } else if (i == 14) {
         card.name = "Ace of Hearts";
-          card.src = imageDir + 'ace_of_hearts';
+          card.src = imageDir + 'ace_of_hearts.png';
       }
       deck.push(card);
     }
@@ -32,22 +32,22 @@ function initDeck () {
       var card = {};
       if (i < 10) {
         card.name = i + ' of Spades';
-        card.src = imageDir + i + '_of_spades';
+        card.src = imageDir + i + '_of_spades.png';
       deck.push(card);
     } else {
       var card = {};
       if (i == 11) {
         card.name = "Jack of Spades";
-        card.src = imageDir + "jack_of_spades";
+        card.src = imageDir + "jack_of_spades.png";
       } else if (i == 12) {
         card.name = "Queen of Spades";
-        card.src = imageDir + "queen_of_spades";
+        card.src = imageDir + "queen_of_spades.png";
       } else if (i == 13) {
         card.name = "King of Spades";
-        card.src = imageDir + "king_of_spades";
+        card.src = imageDir + "king_of_spades.png";
       } else if (i == 14) {
         card.name = "Ace of Spades";
-        card.src = imageDir + "ace_of_spades";
+        card.src = imageDir + "ace_of_spades.png";
       }
       deck.push(card);
     }
@@ -57,22 +57,22 @@ function initDeck () {
     var card = {};
       if (i < 10) {
         card.name = i + ' of Clubs';
-        card.src = imageDir + i + '_of_clubs';
+        card.src = imageDir + i + '_of_clubs.png';
       deck.push(card);
     } else {
       var card = {};
       if (i == 11) {
         card.name = "Jack of Clubs";
-        card.src = imageDir + 'jack_of_clubs';
+        card.src = imageDir + 'jack_of_clubs.png';
       } else if (i == 12) {
         card.name = "Queen of Clubs";
-        card.src = imageDir + 'queen_of_clubs';
+        card.src = imageDir + 'queen_of_clubs.png';
       } else if (i == 13) {
         card.name = "King of Clubs";
-        card.src = imageDir + 'king_of_clubs';
+        card.src = imageDir + 'king_of_clubs.png';
       } else if (i == 14) {
         card.name = "Ace of Clubs";
-        card.src = imageDir + 'ace_of_clubs';
+        card.src = imageDir + 'ace_of_clubs.png';
       }
       deck.push(card);
     }
@@ -82,22 +82,22 @@ function initDeck () {
     var card = {};
       if (i < 10) {
       card.name = i + ' of Diamonds';
-      card.src = imageDir + i + '_of_diamonds'
+      card.src = imageDir + i + '_of_diamonds.png'
       deck.push(card);
     } else {
       var card = {};
       if (i == 11) {
         card.name = "Jack of Diamonds";
-        card.src = imageDir + "jack_of_diamonds";
+        card.src = imageDir + "jack_of_diamonds.png";
       } else if (i == 12) {
         card.name = "Queen of Diamonds";
-        card.src = imageDir + "queen_of_diamonds";
+        card.src = imageDir + "queen_of_diamonds.png";
       } else if (i == 13) {
         card.name = "King of Diamonds";
-        card.src = imageDir + "king_of_diamonds";
+        card.src = imageDir + "king_of_diamonds.png";
       } else if (i == 14) {
         card.name = "Ace of Diamonds";
-        card.src = imageDir + "ace_of_diamonds";
+        card.src = imageDir + "ace_of_diamonds.png";
       }
       deck.push(card);
     }
