a06bc8c2da8d8761754a3df19b2340740046e1ba
onesc <onescyt@gmail.com>
2016-07-15 11:47:40 +1000

    .

--- server.js
+++ server.js
@@ -131,9 +131,7 @@ io.on('connection', function(socket){
 
 });
 //
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(process.env.PORT || 3000);
 
 function generateRoom() {
   var randString = Math.random().toString(36).substring(7);
